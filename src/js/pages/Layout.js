6f674220b2b8583798ef0686aefc84088137033d
levi koskan <levikoskan@gmail.com>
2016-03-13 15:04:10 -0500

    removed components/info from layout

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router";
 
 import Footer from "../components/layout/Footer";
-import Info from "../components/info";
+
 
 
 export default class Layout extends React.Component {
@@ -23,7 +23,7 @@ export default class Layout extends React.Component {
         <div className="container" style={containerStyle}>
           <div className="row">
             <div className="col-lg-1">
-              <Info />
+
             </div>
             <div className="col-lg-10">
               {this.props.children}
