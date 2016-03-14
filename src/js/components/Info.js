968b514471fffd5d5b59eb3908eef1e02c69d277
levi koskan <levikoskan@gmail.com>
2016-03-13 22:20:44 -0500

    changed some stuff in info

--- src/js/components/Info.js
+++ src/js/components/Info.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { IndexLink, Link } from "react-router";
+import { Link } from "react-router";
 
 export default class Info extends React.Component {
   constructor(props) {
@@ -11,33 +11,33 @@ export default class Info extends React.Component {
 
 
   render() {
-    const disclaimer = location.pathname.match(/^\/disclaimer/) ? "active" : "";
-    const linkStyle = { textDecoration: 'none'};
-    const infoBtn = 'infoBox';
+    // const disclaimer = location.pathname.match(/^\/disclaimer/) ? "active" : "";
+    // const linkStyle = { textDecoration: 'none'};
+    // const infoBtn = 'infoBox';
 
       return (
         <div>
-          <Link to="disclaimer" style={linkStyle}>
-          <button className={disclaimer} id="howItWorks" className="infoBox" type="button">
-            <ul className="infoUl">
-              <li className="infoText">H</li>
-              <li className="infoText">O</li>
-              <li className="infoText">W</li>
-            </ul>
-            <ul className="infoUl">
-              <li className="infoText">I</li>
-              <li className="infoText">T</li>
-            </ul>
-            <ul className="infoUl">
-              <li className="infoText">W</li>
-              <li className="infoText">O</li>
-              <li className="infoText">R</li>
-              <li className="infoText">K</li>
-              <li className="infoText">S</li>
-            </ul>
+          // <Link to="disclaimer" style={linkStyle}>
+          // <button className={disclaimer} id="howItWorks" className="infoBox" type="button">
+          //   <ul className="infoUl">
+          //     <li className="infoText">H</li>
+          //     <li className="infoText">O</li>
+          //     <li className="infoText">W</li>
+          //   </ul>
+          //   <ul className="infoUl">
+          //     <li className="infoText">I</li>
+          //     <li className="infoText">T</li>
+          //   </ul>
+          //   <ul className="infoUl">
+          //     <li className="infoText">W</li>
+          //     <li className="infoText">O</li>
+          //     <li className="infoText">R</li>
+          //     <li className="infoText">K</li>
+          //     <li className="infoText">S</li>
+          //   </ul>
 
-          </button>
-            </Link>
+          // </button>
+          //   </Link>
 
         </div>
       );
